dea7e579cf4d9c721642fa1bd48c8c75b961ce09
Genebson <mauryavero@gmail.com>
2021-04-08 16:29:27 -0300

    Fix bugs in challenge 4

--- clase5/tarea4/tarea4.js
+++ clase5/tarea4/tarea4.js
@@ -12,22 +12,71 @@ const $numerosDelArray = document.querySelectorAll('li')
 const $calcular = document.querySelector('#calcular')
 
 let numerosArray = []
-let promedio;
-let numeroPequeño;
-let numeroGrande;
-let numeroRepetido;
 
-for (i = 0; i < $numerosDelArray.length; i++) {
-  numerosArray.push($numerosDelArray[i].innerText)
+function obtenerArray(numerosArray) {
+  for (let i = 0; i < $numerosDelArray.length; i++) {
+    numerosArray.push(Number($numerosDelArray[i].innerText))
+  }
+  return numerosArray
 }
+
+console.log(obtenerArray(numerosArray));
 
 function calcularPromedio(numerosArray) {
   let totalNota = 0;
-  for (i = 0; i < numerosArray.length; i++) {
-    totalNota = totalNota + numerosArray[i]
+  for (let i = 0; i < numerosArray.length; i++) {
+    totalNota += numerosArray[i]
   }
   return totalNota / numerosArray.length
 }
+
+console.log(calcularPromedio(numerosArray));
+
+function obtenerNumeroPequeño(numerosArray) {
+  let numeroMasPequeño = numerosArray[0]
+  for (let i = 0; i < numerosArray.length; i++) {
+    if (numerosArray[i] < numeroMasPequeño) {
+      numeroMasPequeño = numerosArray[i]
+    }
+  }
+  return numeroMasPequeño
+}
+
+console.log(obtenerNumeroPequeño(numerosArray));
+
+function obtenerNumeroGrande(numerosArray) {
+  let numeroMasGrande = numerosArray[0]
+  for (let i = 0; i < numerosArray.length; i++) {
+    if (numerosArray[i] > numeroMasGrande) {
+      numeroMasGrande = numerosArray[i]
+    }
+  }
+  return numeroMasGrande
+}
+
+console.log(obtenerNumeroGrande(numerosArray));
+
+function obtenerNumeroRepetido(numerosArray) {
+  let contadorNumeroActual = 0;
+  let repeticionesMaximas = 0;
+  let numeroMasrepetido;
+
+  for (let i = 0; i < numerosArray.length; i++) {
+    for (let j = 0; j < numerosArray.length; j++) {
+      if (numerosArray[i] == numerosArray[j]) {
+        contadorNumeroActual++
+      }
+      if (repeticionesMaximas < contadorNumeroActual) {
+        repeticionesMaximas = contadorNumeroActual
+        numeroMasrepetido = numerosArray[i]
+      }
+    }
+    contadorNumeroActual = 0
+  }
+  return numeroMasrepetido
+}
+
+console.log(obtenerNumeroRepetido(numerosArray));
 
 $calcular.onclick = function () {
   const promedio = document.querySelector('#promedio-total')
@@ -36,7 +85,7 @@ $calcular.onclick = function () {
   const numeroRepetido = document.querySelector('#numero-repetido')
 
   promedio.innerText = `El promedio es: ${calcularPromedio(numerosArray)}`
-  numeroPequeño.innerText = `El numero más pequeño es: ${numerosArray[5]}`
-  numeroGrande.innerText = `El numero más grande es: ${numerosArray[7]}`
-  numeroRepetido.innerText = `El numero más frecuente es: ${numerosArray[3]}`
+  numeroPequeño.innerText = `El numero más pequeño es: ${obtenerNumeroPequeño(numerosArray)}`
+  numeroGrande.innerText = `El numero más grande es: ${obtenerNumeroGrande(numerosArray)}`
+  numeroRepetido.innerText = `El numero más frecuente es: ${obtenerNumeroRepetido(numerosArray)}`
 }
