ae821d7f765211768da4f8fd9262c4ce8d168baa
Genebson <mauryavero@gmail.com>
2021-04-20 20:07:29 -0300

    Actualiza funcion calcularSalario

--- clase6/tarea2/tarea2.js
+++ clase6/tarea2/tarea2.js
@@ -11,6 +11,7 @@ Punto bonus: si hay inputs vacíos, ignorarlos en el cálculo (no contarlos como
 const $botonAgregar = document.querySelector('#agregar-sueldo')
 const $botonQuitar = document.querySelector('#quitar-sueldo')
 const $botonCalcular = document.querySelector('#calcular-salario')
+const $botonResetear = document.querySelector('#resetear')
 
 $botonAgregar.onclick = function () {
   agregarInput()
@@ -22,40 +23,88 @@ $botonQuitar.onclick = function () {
 
 $botonCalcular.onclick = function () {
   const salarioAnual = calcularSalario()
+  mostrarBotonResetear()
+  ocultarBotonCalcular()
   crearTextoResultado(salarioAnual)
+}
+
+$botonResetear.onclick = function () {
+  resetear()
+  mostrarBotonCalcular()
 }
 
 function agregarInput() {
   const $divSueldos = document.querySelector('#div-sueldos')
   const $crearInput = document.createElement('input')
+
+  const $div = document.createElement('div')
+  $div.className = 'sueldos-input'
+
   $crearInput.type = "number"
   $crearInput.placeholder = 'Ingrese su sueldo'
   $crearInput.className = 'inputs'
-  $divSueldos.appendChild($crearInput)
-  $divSueldos.appendChild(document.createElement('br'))
+
+  $div.appendChild($crearInput)
+  $div.appendChild(document.createElement('br'))
+  $divSueldos.appendChild($div)
 }
 
 function quitarInput() {
-  const $removerInput = document.querySelectorAll('.inputs')
-  for (let i = 0; i < $removerInput.length; i++) {
-    $removerInput[i].remove()
-  }
+  const $removerInput = document.querySelectorAll('.sueldos-input')
+  const ultimoInput = $removerInput.length - 1
+  $removerInput[ultimoInput].remove()
 }
 
 function crearTextoResultado(salarioAnual) {
-  const $p = document.querySelector('#texto-resultado')
-  $p.appendChild(document.createTextNode(
-    `El Salario mayor en la familia es de: ${obtenerSalarioMayor(salarioAnual)} 
+  const $divResultado = document.querySelector('#resultado')
+  const $p = document.createElement('p')
+  $p.className = 'texto-p'
+  $p.textContent = `El Salario mayor en la familia es de: ${obtenerSalarioMayor(salarioAnual)} 
     El Salario menor en la familia es de: ${obtenerSalarioMenor(salarioAnual)} 
-    El Salario promedio de la familia es de: ${obtenerPromedio(salarioAnual)}`))
+    El Salario promedio de la familia es de: ${obtenerPromedio(salarioAnual)}`
 
+  $divResultado.appendChild($p)
+}
+
+function borrarDatos() {
+  const inputSueldos = document.querySelectorAll('.sueldos-input')
+  document.querySelector('.texto-p').remove()
+
+  for (let i = 0; i < inputSueldos.length; i++) {
+    inputSueldos[i].remove()
+  }
 }
 
 function calcularSalario() {
   const $salariosInput = document.querySelectorAll('.inputs')
   let salarioArray = []
   for (let i = 0; i < $salariosInput.length; i++) {
+    if ($salariosInput[i].value == '') {
+      continue
+    }
     salarioArray.push(Number($salariosInput[i].value))
   }
   return salarioArray
+}
+
+function ocultarBotonCalcular() {
+  document.querySelector('#calcular-salario').className = 'btn-cargar oculto'
+}
+
+function mostrarBotonCalcular() {
+  document.querySelector('#calcular-salario').className = 'btn-cargar'
+}
+
+function ocultarBotonResetear() {
+  document.querySelector('#resetear').className = 'btn-cargar oculto'
+}
+
+function mostrarBotonResetear() {
+  document.querySelector('#resetear').className = 'btn-cargar'
+}
+
+function resetear() {
+  borrarDatos()
+  ocultarBotonCalcular()
+  ocultarBotonResetear()
 }
